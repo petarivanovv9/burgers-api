774deaf214b2433a5cbf837bfb621c6a8182d37b
Petar Ivanov <pepincho95@gmail.com>
2017-11-08 20:01:35 +0200

    Add initial pagination functionality

--- controllers/burger.js
+++ controllers/burger.js
@@ -11,6 +11,16 @@ exports.list_all_burgers = function(req, res, next) {
       errorMessage: 'Must have a value and if you are using multiple words use underscores to separate',
       optional: true,
       notEmpty: true
+    },
+    page: {
+      errorMessage: 'Must be a number greater than 0',
+      optional: true,
+      isInt: { options: { min: 1 } }
+    },
+    per_page: {
+      errorMessage: 'Must be a number greater than 0 and less than 80',
+      optional: true,
+      isInt: { options: { min: 1, max: 80 } }
     }
   });
 
@@ -20,14 +30,10 @@ exports.list_all_burgers = function(req, res, next) {
     return next(validationError(errors));
   }
 
-  if ( ! req.query.burger_name ) {
-    Burger.find({}, function(err, burgers) {
-      if (err)
-        res.send(err);
 
-      res.status(200);
-      res.json(burgers);
-    });
+
+  if ( ! req.query.burger_name ) {
+    pagination(req, res);
   } else {
     read_a_burger_by_name(req, res);
   }
@@ -106,5 +112,26 @@ function read_a_burger_by_name(req, res) {
 
     res.status(200);
     res.json(burger);
+  });
+}
+
+
+function pagination(req, res) {
+  var { page, per_page } = req.query;
+
+  var per_page    = per_page || 25;
+  var page_number = page || 1;
+
+  var parsed_per_page    = parseInt(per_page);
+  var parsed_page_number = parseInt(page_number);
+
+  var offset = (parsed_page_number - 1) * parsed_per_page;
+
+  Burger.find({}, {}, { skip: offset, limit: parsed_per_page }, function(err, burgers) {
+    if (err)
+      res.send(err);
+
+    res.status(200);
+    res.json(burgers);
   });
 }
