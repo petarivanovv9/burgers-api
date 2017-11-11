c6884e6b73a277c3046a884539f69833916dac0c
Petar Ivanov <pepincho95@gmail.com>
2017-11-11 13:09:39 +0200

    Add are_all_query_params_valid function

--- controllers/burger.js
+++ controllers/burger.js
@@ -15,6 +15,10 @@ exports.list_all_burgers = function(req, res, next) {
 
   var errors = req.validationErrors();
 
+
+  if (are_all_params_valid(req.query))
+    return next(validationError())
+
   if (errors)
     return next(validationError(errors));
 
@@ -109,4 +113,19 @@ function pagination(req, res) {
     res.status(200);
     res.json(burgers);
   });
+}
+
+
+function are_all_params_valid(req_query) {
+  var req_query_size = Object.keys(req_query).length;
+  var valid_params = ['burger_name', 'per_page', 'page'];
+  var valid_params_size = valid_params.length;
+  var counter = 0;
+
+  for (var i = 0; i < valid_params_size; i++) {
+    if (valid_params[i] in req_query)
+      counter += 1;
+  }
+
+  return counter != req_query_size;
 }
